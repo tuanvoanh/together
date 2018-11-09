acc0ed632d64578585835145109d36f7267bedb2
tuanvoanh <94voanhtuan@gmail.com>
2018-11-09 08:52:35 +0700

    Add files via upload

---
+++ focus_clock_network.ino
@@ -0,0 +1,29 @@
+#include "Network.h"
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+  wifiInit();
+  mqttInit();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  mqttLoop();
+  if (timeSetAvailable()){
+    Serial.print("Set time :");
+    Serial.print(hourSetRead());
+    Serial.print(":");
+    Serial.println(minuteSetRead());
+  }
+  if (alarmAvailable()){
+    Serial.print("Set Alarm :");
+    Serial.print(alarmHourRead());
+    Serial.print(":");
+    Serial.println(alarmMinuteRead());
+  }
+  if (circleAvailable()){
+    Serial.print("Set circle :");
+    Serial.print(circleRead());
+  }
+}
